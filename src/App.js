a50b4c1af4916fa83ee6d5c531e2c481bbba75d9
James Manalang <jaesmanalang@gmail.com>
2020-02-24 01:24:14 +0800

    reset / broken into smaller components, finished styling the menu-item component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/HomePage';
 import './App.css';
-
-import HomePage from './pages/hompage/homepage.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
-      </Switch>
+      <HomePage />
     </div>
   );
 }
 
 export default App;
-
-// 11. styling menu-items
